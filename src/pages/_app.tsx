e0609ce81d7cc64edccf16e34ed3e55f7b3c8472
Gian Trotta <giantrotta24@gmail.com>
2022-07-17 14:06:40 -0400

    remove loading useEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,9 @@
 import '../styles/globals.css';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 
-import LoadingContainer from '../components/common/Loading';
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    const handleStart = (url) => {
-      console.log(`Loading: ${url}`);
-      setLoading(true);
-    };
-    const handleStop = () => {
-      setLoading(false);
-    };
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleStop);
-    router.events.on('routeChangeError', handleStop);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleStop);
-      router.events.off('routeChangeError', handleStop);
-    };
-  }, [router]);
-
-  return <>{loading ? <LoadingContainer /> : <Component {...pageProps} />}</>;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
